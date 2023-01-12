19dcb28a67211aad0bf07bcdbdfd16af4ba61dd4
fcojavierrodriguezcanal <franciscojavierrodriguez@adaits.es>
2023-01-12 12:11:55 +0100

    primer ejercicio tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'nunito': ['nunito', 'sans-serif'],
+            'MyFont': ['"My Font"', 'serif'] 
+          },
+        variants:{},
+        plugins: []
+    }
+    
+}
