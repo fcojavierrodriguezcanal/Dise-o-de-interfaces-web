8c4257904eb2039f825400b18a2e1d35a0dac20f
fcojavierrodriguezcanal <franciscojavierrodriguez@adaits.es>
2023-01-16 13:46:07 +0100

    yo mas no puedo

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,16 +1,52 @@
+
 module.exports = {
-    theme: {
-        extends: {
-            fontFamily: {
-                'nunito': ['nunito', 'sans-serif'],
-                'MyFont': ['"My Font"', 'serif'] 
-            },
-            colors: {
-                
-            },
-            variants:{},
-            plugins: []
-        }
-    }
-    
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['Bebas Neue', 'serif'],
+      },
+
+      colors: {
+        'blue-ocean': '#37bcf9',
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        purple: '#3f3cbb',
+        midnight: '#121063',
+        metal: '#565584',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        'bubble-gum': '#ff77e9',
+        bermuda: '#78dcca',
+        gray: '#ccc',
+      },
+      keyframes: {
+        rotategear: {
+          from: { transform: 'rotateZ(0deg)' },
+          to: { transform: 'rotateZ(360deg)' },
+        },
+        fromBelow: {
+          '0%': {
+            transform: 'translateY(0%)',
+          },
+          '50%': {
+            transform: 'translateY(200%)',
+          },
+          '100%': {
+            transform: 'translateY(0%)',
+          },
+        },
+        fromRight: {
+          from: { transform: ' translateX(100%)' },
+          to: { transform: ' translateX(0)' },
+        },
+      },
+      animation: {
+        rotategear: 'rotategear 1s infinite linear',
+        fromBelow: 'fromBelow 500ms linear',
+        fromRight: 'fromRight 300ms linear alternate',
+      },
+    },
+    plugins: [],
+  },
 }
