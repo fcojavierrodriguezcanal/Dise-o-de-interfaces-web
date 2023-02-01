e7f0f1d6b8aa79666d62b1e6439aab2b46bb405e
fcojavierrodriguezcanal <95092710+fcojavierrodriguezcanal@users.noreply.github.com>
2023-02-01 20:31:50 +0100

    retoques

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       borderWidth: {
         '10': '10px',
       },
+      boxShadow: {
+        'cartashadow': '1px 1px 2px #ddd',
+      },
       colors: {
         'blue-ocean': '#37bcf9',
         transparent: 'transparent',
@@ -51,11 +54,52 @@ module.exports = {
           from: { transform: ' translateX(100%)' },
           to: { transform: ' translateX(0)' },
         },
+        showCategory: {
+          from: {
+              transform: 'translateY(-300%) color:black',
+              
+          },
+          to: {
+              transform: 'translateY(0%) color: white',
+              
+          },
+        },
+        showDescription: {
+          from: {
+            transform: 'translateX(-300%)',
+            color: 'black',
+          },
+          to: {
+            transform: 'translateX(0%)',
+          }
+        },
+        backBanner: {
+            '0%': {
+              'background-position': '0 0',
+            },
+            '100%': {
+              'background-position': '-200px -200px',
+            },
+          },
+          showIcon: {
+            from: {
+              transform: 'translateY(-200%)',
+            },
+            to: {
+              transform: 'translateY(0%)'
+            }
+          },
+
+
+      
       },
       animation: {
         rotategear: 'rotategear 1s infinite linear',
         fromBelow: 'fromBelow 500ms linear',
         fromRight: 'fromRight 300ms linear alternate',
+        showCategory: 'showCategory 400ms linear',
+        backBanner: 'backBanner 10s linear',
+        showDescription: 'showDescription 400ms linear',
       },
     },
     plugins: [],
